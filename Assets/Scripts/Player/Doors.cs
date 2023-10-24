3fdd711b391539ad22ce4678c49ffeade847893e
JamesAmigo <jijia66@gmail.com>
2023-10-24 00:58:29 -0400

    Episode 1 implementaion
    Created a new scene called episode 1
    Added a new smaller meeting room
    Changed some layout of the office
    Door minor change

--- Assets/Scripts/Player/Doors.cs
+++ Assets/Scripts/Player/Doors.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 namespace INTENT
 {
@@ -12,6 +13,9 @@ namespace INTENT
         [Header("GameObject Components")]
         [Tooltip("The teleport location for the player")]
         [SerializeField] private Transform teleportTrans;
+        
+        [SerializeField] private TextMeshPro doorNameTag1;
+        [SerializeField] private TextMeshPro doorNameTag2;
         public Transform TeleportTrans { get { return teleportTrans; } }
         #endregion
 
@@ -20,6 +24,9 @@ namespace INTENT
         [Header("Door Settings")]
         [Tooltip("The door this door is connected to")]
         [SerializeField] private Doors targetDoor;
+
+        [Tooltip("The target room name")]
+        [SerializeField] private string targetRoomName;
         #endregion
 
 
@@ -34,6 +41,12 @@ namespace INTENT
             }
         }
         #endregion
+
+        private void OnValidate() 
+        {
+            doorNameTag1.text = targetRoomName;
+            doorNameTag2.text = targetRoomName;
+        }
 
         private void OnTriggerEnter(Collider other)
         {
