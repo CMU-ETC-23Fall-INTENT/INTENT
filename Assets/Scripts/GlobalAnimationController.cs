aa62d01d16a341e955eefc242684cbc6babb7db6
JamesAmigo <jijia66@gmail.com>
2023-10-26 12:25:44 -0400

    Animations

---
+++ Assets/Scripts/GlobalAnimationController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+namespace INTENT
+{
+    public class GlobalAnimationController : MonoBehaviour
+    {
+
+        [SerializeField]
+        int animationID;
+
+        public Animator playerAlexAnimator, tonyAnimator, blakeAnimator, rileyAnimator;
+        
+
+        [YarnCommand ("PlayAnimation")]
+        public void PlayAnimation(string characterName, int stateID)
+        {            
+            switch (characterName)
+            {
+                case "Alex":
+                    playerAlexAnimator.SetTrigger(playerAlexAnimator.parameters[stateID - 1].name);
+                    break;
+
+                case "Tony":
+                    tonyAnimator.SetTrigger(playerAlexAnimator.parameters[stateID - 1].name);
+                    break;
+
+                case "Blake":
+                    blakeAnimator.SetTrigger(playerAlexAnimator.parameters[stateID - 1].name);
+                    break;
+                case "Riley":
+                    rileyAnimator.SetTrigger(playerAlexAnimator.parameters[stateID - 1].name);
+                    break;
+            }
+        }
+
+    }
+
+
+
+
+}
