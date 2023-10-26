1e1c093c2466f8dd3c13a7eff3c0a1cec65e5ade
Zibo Ye <32037915+zibo-ye@users.noreply.github.com>
2023-10-26 00:55:13 -0400

    Compatibility with the plugin

--- Assets/Scripts/EventSystem/TaskEvents.cs
+++ Assets/Scripts/EventSystem/TaskEvents.cs
@@ -1,41 +1,44 @@
 using System;
-
-// This class is used to define task events that can be subscribed to by other classes
-public class TaskEvents
+namespace INTENT
 {
-    // This event is used when the task is available to be interacted with
-    public event Action<string> OnTaskAvailable;
-
-    public void TaskAvailable(string taskId)
+    // This class is used to define task events that can be subscribed to by other classes
+    public class TaskEvents
     {
-        OnTaskAvailable?.Invoke(taskId);
+        // This event is used when the task is available to be interacted with
+        public event Action<string> OnTaskAvailable;
+
+        public void TaskAvailable(string taskId)
+        {
+            OnTaskAvailable?.Invoke(taskId);
+        }
+
+
+        // This event is used when the task is started  
+        public event Action<string> OnTaskStarted;
+
+        public void TaskStarted(string taskId)
+        {
+            OnTaskStarted?.Invoke(taskId);
+        }
+
+
+        // This event is used when the task is completed
+        public event Action<string> OnTaskCompleted;
+
+        public void TaskCompleted(string taskId)
+        {
+            OnTaskCompleted?.Invoke(taskId);
+        }
+
+
+        // This event is used when the task status is changed
+        public event Action<Task> OnTaskStatusChanged;
+
+        public void TaskStatusChanged(Task task)
+        {
+            OnTaskStatusChanged?.Invoke(task);
+        }
     }
 
 
-    // This event is used when the task is started  
-    public event Action<string> OnTaskStarted;
-
-    public void TaskStarted(string taskId)
-    {
-        OnTaskStarted?.Invoke(taskId);
-    }
-
-
-    // This event is used when the task is completed
-    public event Action<string> OnTaskCompleted;
-
-    public void TaskCompleted(string taskId)
-    {
-        OnTaskCompleted?.Invoke(taskId);
-    }
-    
-
-    // This event is used when the task status is changed
-    public event Action<Task> OnTaskStatusChanged;
-
-    public void TaskStatusChanged(Task task)
-    {
-        OnTaskStatusChanged?.Invoke(task);
-    }
 }
-
