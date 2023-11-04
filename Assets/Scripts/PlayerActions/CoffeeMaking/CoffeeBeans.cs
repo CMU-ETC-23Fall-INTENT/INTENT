0d7f2ed467f9b4160321a8ea44d40a1d2c363a20
JamesAmigo <jijia66@gmail.com>
2023-11-04 11:28:50 -0400

    New Coffee Interaction Change

--- Assets/Scripts/PlayerActions/CoffeeMaking/CoffeeBeans.cs
+++ Assets/Scripts/PlayerActions/CoffeeMaking/CoffeeBeans.cs
@@ -1,20 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace INTENT
 {
-    public class CoffeeBeans : MonoBehaviour, IDragHandler
+    public class CoffeeBeans : MonoBehaviour, IDragHandler, IEndDragHandler
     {
         private Vector3 mousePosition;
         private Camera mainCamera;
+        private float originY;
+        private bool onHand;
+        [SerializeField] private GameObject moveArea;
         [SerializeField] private LayerMask moveLayer;
         // Start is called before the first frame update
         void Start()
         {
             mainCamera = Camera.main;
+            originY = transform.position.y;
+            moveArea.SetActive(false);
         }
 
         private void OnTriggerEnter(Collider other) 
@@ -22,6 +28,7 @@ namespace INTENT
             if(other.CompareTag("CoffeeMachine"))
             {
                 other.GetComponent<CoffeeMachine>().BeanIn();
+                moveArea.SetActive(false);
                 Destroy(this.gameObject);  
             }
         }
@@ -31,10 +38,22 @@ namespace INTENT
         {
             Ray ray = mainCamera.ScreenPointToRay(eventData.position);
             RaycastHit hit;
+            moveArea.SetActive(true);
             if(Physics.Raycast(ray, out hit, 100f, moveLayer))
             {
                 transform.position = hit.point;
             }
         }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            moveArea.SetActive(false);
+            transform.position = new Vector3(transform.position.x, originY, transform.position.z);
+        }
+
+        
+
+        
+        
     }
 }
