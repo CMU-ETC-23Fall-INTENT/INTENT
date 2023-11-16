30d9f94ee67f45944aa89c35613c8e0baaaed297
JamesAmigo <jijia66@gmail.com>
2023-11-16 02:27:31 -0500

    Agenda interaction

---
+++ Assets/Scripts/PlayerActions/AgendaAction/ActionAgenda.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace INTENT
+{
+    public class ActionAgenda : PlayerAction
+    {
+        [Header("Desktop Page")]
+        [SerializeField] private GameObject desktopPage;
+        [SerializeField] private GameObject confirmReservePanel;
+
+        [Header("Agenda Page")]
+        [SerializeField] private GameObject agendaPage;
+        [SerializeField] private GameObject greyAgendaComfirmButton;
+        [SerializeField] private GameObject blueAgendaComfirmButton;
+
+        [Header("Reservation Page")]
+        [SerializeField] private GameObject reservationPage;
+        [SerializeField] private GameObject greyReservationButton;
+        [SerializeField] private GameObject blueReservationButton;
+        [SerializeField] private GameObject reminderPanel;
+
+
+        private GameObject currentPage;
+        private int taskCount = 0;
+        private int reservedRoom = 0;
+        private void OnEnable() 
+        {
+            GameManager.Instance.PlayerEnterAction();
+            currentPage = desktopPage;
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+        public void OpenPage(GameObject page)
+        {
+            currentPage.SetActive(false);
+            currentPage = page;
+            currentPage.SetActive(true);
+        }
+        public void DragTaskCount(int count)
+        {
+            taskCount += count;
+            Debug.Log("Task count " + taskCount);
+            if(taskCount == 3)
+            {
+                greyAgendaComfirmButton.SetActive(false);
+                blueAgendaComfirmButton.SetActive(true);    
+            }
+            else
+            {
+                greyAgendaComfirmButton.SetActive(true);
+                blueAgendaComfirmButton.SetActive(false);    
+            }
+        }
+        public void RoomButton(int roomNumber)
+        {
+            reservedRoom = roomNumber;
+            greyReservationButton.SetActive(false);
+            blueReservationButton.SetActive(true);
+        }
+        public void ReminderOKButton()
+        {
+            reminderPanel.SetActive(false);
+            reservedRoom = 0;
+            greyReservationButton.SetActive(true);
+            blueReservationButton.SetActive(false);
+        }
+        public void ConfirmReservationButton()
+        {
+            if(reservedRoom == 1)
+            {
+                reminderPanel.SetActive(true);
+            }
+            else if(reservedRoom == 2)
+            {
+                OpenPage(desktopPage);
+                confirmReservePanel.SetActive(true);
+                StartCoroutine(DelayedPerform(2f));
+            }
+        }
+        public override void PerformAction()
+        {
+            GameManager.Instance.PlayerExitAction();
+            SuccessFinishAction();
+        }
+        IEnumerator DelayedPerform(float sec)
+        {
+            yield return new WaitForSeconds(sec);
+            PerformAction();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerActions/AgendaAction/DraggableTask.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace INTENT
+{
+    public class DraggableTask : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+    {
+        public int TaskID;
+        [SerializeField] private ActionAgenda actionAgenda;
+        [SerializeField] private GameObject backgroundParent;
+        private bool isIn;
+        
+        public void TaskDragedIn(bool draggedIn)
+        {
+            GetComponent<CanvasGroup>().alpha = draggedIn ? 0f : 1f;
+            isIn = draggedIn;
+        }
+        
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = Input.mousePosition;
+            if(isIn)
+                actionAgenda.DragTaskCount(-1);
+            
+            TaskDragedIn(false);
+        }
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+            if(isIn)
+            {
+                transform.parent.GetComponent<TaskSlots>().RemoveTask();
+            }
+            
+            transform.parent = backgroundParent.transform;
+        }
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+    }
+}
