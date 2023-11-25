09a95967124be6e423b3f9b0e5d271b1056a3223
JamesAmigo <jijia66@gmail.com>
2023-11-25 01:44:26 -0500

    - Prevent seeing Tony go out of conference room - Minor change to transition function to take episode

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -92,8 +92,7 @@ namespace INTENT
             if(transitToEP2)
             {
                 transitToEP2 = false;
-                StartCoroutine(ElevatorTransitionController.EpisodeTransition("One Month Later...", 2f));
-                TaskManager.Instance.ActivateEpisode(Episode.Episode2);
+                StartCoroutine(ElevatorTransitionController.EpisodeTransition("One Month", " Later...", 2f, Episode.Episode2));
             }
         }
         public void OpenLearnPanel(bool open)
