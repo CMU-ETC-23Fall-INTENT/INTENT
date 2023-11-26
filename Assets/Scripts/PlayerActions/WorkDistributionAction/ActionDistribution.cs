a27fd267a155a75a0b6097404cd4aebc0343edf3
JamesAmigo <jijia66@gmail.com>
2023-11-26 00:31:09 -0500

    Disable tonemapping for distribution

--- Assets/Scripts/PlayerActions/WorkDistributionAction/ActionDistribution.cs
+++ Assets/Scripts/PlayerActions/WorkDistributionAction/ActionDistribution.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using Unity.VisualScripting;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
 
 namespace INTENT
 {
     public class ActionDistribution : PlayerAction
     {
+        [SerializeField] private GameObject globalVolume;
+        private VolumeProfile volumeProfile;
         [SerializeField] private CinemachineVirtualCamera virtualCamera;
         [SerializeField] private GameObject defaultImage;
         [SerializeField] private GameObject distributionImage;
@@ -34,9 +38,14 @@ namespace INTENT
         }
         private void OnEnable()
         {
+            
             GameManager.Instance.PlayerEnterAction();
             virtualCamera.Priority = 11;
             StartCoroutine(StartWhiteBoard(1f));
+        }
+        private void Awake() 
+        {
+            volumeProfile = globalVolume.GetComponent<Volume>().profile;
         }
         public override void ResetAction()
         {
@@ -56,6 +65,10 @@ namespace INTENT
         }
         public override void PerformAction()
         {
+            if(volumeProfile.TryGet(out Tonemapping tonemapping))
+            {
+                tonemapping.mode.value = TonemappingMode.Neutral;
+            }
             GameManager.Instance.PlayerExitAction();
             virtualCamera.Priority = 9;
             SuccessFinishAction();
@@ -66,6 +79,7 @@ namespace INTENT
         }
         IEnumerator WaitAndPerformAction(float sec)
         {
+
             doneImage.SetActive(true);
             yield return new WaitForSeconds(sec);
             PerformAction();
@@ -73,7 +87,10 @@ namespace INTENT
         IEnumerator StartWhiteBoard(float sec)
         {
             yield return new WaitForSeconds(1.5f);
-
+            if(volumeProfile.TryGet(out Tonemapping tonemapping))
+            {
+                tonemapping.mode.value = TonemappingMode.None;
+            }
             float timer = 0f;
             while(timer < sec)
             {
